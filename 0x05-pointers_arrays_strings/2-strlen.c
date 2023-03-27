8de1383f8d1784968ce4ee8be70ab9174b1bfbe2
Seleemmiah <seleemmiah1@gmail.com>
2023-03-27 15:15:35 +0100

    Pointers and array

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ *  * _strlen - returns the length of a string
+ *   * @s: Pointer for the string
+ *    * Return: Always 0
+ *     */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *  * print_rev - prints a string, in reverse
+ *   * @s: the string pointer
+ *    * Return: Always 0
+ *     */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		putchar(s[i]);
+	}
+        _putchar('\n');
+}
