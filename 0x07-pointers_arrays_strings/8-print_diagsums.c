d13250e837f8aee8142890b03255340bc96ef297
Seleemmiah <seleemmiah1@gmail.com>
2023-04-03 16:30:40 -0700

    Even pointers updated

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,7 +13,7 @@ void print_diagsums(int *a, int size)
 	sum1 = 0;
        	sum2 = 0;
 
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		sum1 += *(a + i * size + i);
 		sum2 += *(a + i * size + (size - i - 1));
